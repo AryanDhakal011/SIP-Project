7c816be856502fce1055e639d495f00c8bdb1b53
zerobase01 <118020576+zerobase01@users.noreply.github.com>
2024-03-12 10:51:57 +0545

    updated-viewpassword, input-width

--- javascript/sign-up.js
+++ javascript/sign-up.js
@@ -1,1 +1,14 @@
-  
+const togglePasswordIcons = document.querySelectorAll('.togglePassword');
+togglePasswordIcons.forEach(icon => {
+    icon.addEventListener('click', function (e) {
+        const targetId = this.getAttribute('data-target');
+        const passwordField = document.getElementById(targetId);
+
+        // toggle the type attribute
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+
+        // toggle the eye slash icon
+        this.classList.toggle('fa-eye-slash');
+    });
+});
